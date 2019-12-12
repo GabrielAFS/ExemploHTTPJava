3dec07eb19c4bc17eb2378318b51a4faaa606950
Ubuntu <gabriel.silva@dcomp.ufs.br>
2019-12-12 00:17:14 +0000

    Anotações da aula

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ExemploHTTPJava
 Exemplo simples de cliente HTTP em Java usando sockets
+
+<hr/>
+
+## Anotações da aula
+
+- **HTTP 0.9:** somente requisições GET de páginas HTML.
+
+- **HTTP 1.0:**
+>> Métodos: GET, POST, HEAD;
+>> Tranferência de metadados: **Parâmetros de requisição e resposta**.
+
+- **HTTP 1.1:**
+>> Novos métodos: PUT, DELETE e TRACE;
+>> Reciclagem da conexão;
+>> Resposta em **chunks**.
+
+- **HTTP 2.0:**
+>> Melhoria de desempenho;
+>> **Multiplexação das requisições**;
+>> Camada de frame antes entre a de Aplicação e Sessão.
--- src/main/java/br/ufs/dcomp/ExemploHTTP/HTTPClient2.java
+++ src/main/java/br/ufs/dcomp/ExemploHTTP/HTTPClient2.java
@@ -24,6 +24,7 @@ public class HTTPClient2
         
         String msg = "GET /sites/default/files/logo_unicamp_novo_0.png HTTP/1.1" + CRLF +
                      "Host: www.ic.unicamp.br" + CRLF +
+                     "Connection: close" + CRLF + 
                      CRLF;
                      
         out.write(msg.getBytes());
@@ -48,13 +49,13 @@ public class HTTPClient2
          */    
 
         // Arquivo onde o conteúdo da resposta será escrito
-        File file = new File("/home/ubuntu/environment/sistemas-distribuidos/ExemploHTTPJava/logo_unicamp_novo_0.png");
+        File file = new File("/home/ubuntu/environment/SD/ExemploHTTPJava/logo_unicamp_novo_0.png");
         FileOutputStream fos = new FileOutputStream(file);
         
         System.out.println("Downloading...");
-        byte[] buf = new byte[1000];
+        byte[] buf = new byte[1024];
         int len;
-        while ((len = in.read(buf)) > 0){
+        while ((len = in.read(buf)) != -1){
             fos.write(buf, 0, len);
             fos.flush();
             System.out.println(len);
